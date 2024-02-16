9b4106cebe9a6a9910e1a42a9ecbc5620e5f2897
Tracey Jaquith <tracey@archive.org>
2024-02-16 00:42:01 -0800

    comments, re-indents, etc (nothing substantitive)

--- test/example.test.js
+++ test/example.test.js
@@ -1,13 +1,11 @@
 // eslint-disable-next-line object-curly-newline
 import { describe, it, expect, run } from 'https://raw.githubusercontent.com/internetarchive/dyno/main/test/test.js'
 
-
 describe('test imports', () => {
   it('importing', () => {
     expect(typeof expect).toEqual('function')
     expect(typeof nope).toEqual('undefined')
   })
 })
-
 
 run()
